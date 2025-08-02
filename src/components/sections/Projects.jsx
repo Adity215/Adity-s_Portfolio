ee301a1109b887d641ddead809c38b75bd439fa8
Adity Bharti <aditybharti2114@gmail.com>
2025-08-02 18:15:10 +0530

    inital commit

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -32,12 +32,12 @@ export const Projects = () => {
                     </div>
 
                     <div className="p-6 rounde-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-                        <h3 className="text-lg font-bold mb-2">Project 2</h3>
+                        <h3 className="text-lg font-bold mb-2">Mental Health Support Platform</h3>
                         <p className="text-gray-400 mb-4">
-                            DESCRIPTION
+                            A web-based platform designed to provide mental health support through resources, self-assessment tools, and community interaction. The project aims to raise awareness, reduce stigma, and offer a safe space for users seeking help or information related to mental well-being.
                         </p>
                         <div className="flex flex-wrap gap-2">
-                            {["React", "Node.js", "MongoDB", "Express.js"].map((tech, key) => (
+                            {["React", "Node.js", "SQL", "Express.js", "Python"].map((tech, key) => (
                             <span
                                 key={key}
                                 className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-md transition"
@@ -48,17 +48,17 @@ export const Projects = () => {
                         </div>
 
                         <div className="flex justify-between items-center">
-                            <a href="https://podcast-streaming-2.onrender.com" className="text-blue-400 hover:text-blue-300 transition-colors my-4"> View Project → </a>
+                            
                         </div>
                     </div>
 
                     <div className="p-6 rounde-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-                        <h3 className="text-lg font-bold mb-2">Project 3</h3>
+                        <h3 className="text-lg font-bold mb-2">Weather Website</h3>
                         <p className="text-gray-400 mb-4">
-                            DESCRIPTION
+                            A responsive weather web application using HTML, CSS, and JavaScript that displays real-time weather conditions and a five-day forecast for any city. The app fetches live data such as temperature, humidity, wind speed, and weather conditions through an API and presents it with a clean, us
                         </p>
                         <div className="flex flex-wrap gap-2">
-                            {["React", "Node.js", "MongoDB", "Express.js"].map((tech, key) => (
+                            {["HTML", "CSS", "JS"].map((tech, key) => (
                             <span
                                 key={key}
                                 className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-md transition"
@@ -69,17 +69,17 @@ export const Projects = () => {
                         </div>
 
                         <div className="flex justify-between items-center">
-                            <a href="https://podcast-streaming-2.onrender.com" className="text-blue-400 hover:text-blue-300 transition-colors my-4"> View Project → </a>
+                            
                         </div>
                     </div>
 
                     <div className="p-6 rounde-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-                        <h3 className="text-lg font-bold mb-2">Project 4</h3>
+                        <h3 className="text-lg font-bold mb-2">AI-Powered Threat Intelligence Feed Aggregator</h3>
                         <p className="text-gray-400 mb-4">
-                            DESCRIPTION
+                            AI-Powered Threat Intelligence Feed Aggregator that collects threat feeds (CISA, Microsoft, community blogs), extracts IOCs (IPs, domains, hashes, URLs, CVEs) using regex, and applies AI for summaries, trend analysis, and severity scoring. Features a real-time glassmorphism dashboard, JSON export, search/filter, and live threat simulation.
                         </p>
                         <div className="flex flex-wrap gap-2">
-                            {["React", "Node.js", "MongoDB", "Express.js"].map((tech, key) => (
+                            {["React", "Node.js", "Express.js"].map((tech, key) => (
                             <span
                                 key={key}
                                 className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-md transition"
@@ -90,7 +90,7 @@ export const Projects = () => {
                         </div>
 
                         <div className="flex justify-between items-center">
-                            <a href="https://podcast-streaming-2.onrender.com" className="text-blue-400 hover:text-blue-300 transition-colors my-4"> View Project → </a>
+                            
                         </div>
                     </div>
 
